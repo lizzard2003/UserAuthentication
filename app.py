6be6aa017500c60257aed71e197aaeb7d851a561
Liz DeLaRosa <59484240+lizzard2003@users.noreply.github.com>
2025-10-09 00:58:31 -0500

    DB changes

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template, url_for
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+import os
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
-db = SQLAlchemy(app)  # Creates the database instance
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"\
-app.config['SECRET_KEY']= 'thisisasecretkey'
+
+app.secret_key = "my_secret_key"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+    "DATABASE_URL", "sqlite:///database.db"
+)
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
 
 
 @app.route("/")
@@ -24,4 +36,6 @@ def register():
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
