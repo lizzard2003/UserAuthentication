860a3f88d60e927afc9ed56a0e70720e86c70425
Liz DeLaRosa <59484240+lizzard2003@users.noreply.github.com>
2025-10-13 22:49:17 -0500

    added sql lite

--- app.py
+++ app.py
@@ -1,23 +1,57 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
 import os
-from flask import Flask, render_template
+from flask_bcrypt import Bcrypt
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.path.join(BASE_DIR, 'test.db')}"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 
-app.secret_key = "my_secret_key"
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
-    "DATABASE_URL", "sqlite:///database.db"
-)
 
+app.config["SECRET_KEY"] = "thisisthesecretkey"
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+    # Create the database tables
+
+
+with app.app_context():
+    db.create_all()
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField(
+        validators=[InputRequired(), Length(min=4, max=20)],
+        render_kw={"placeholder": "Password"},
+    )
+    submit = SubmitField("Register")
+
+    def validate_username(seld, username):
+        existing_user_username = User.query.filter_by(username=username.data).first()
+        if existing_user_username:
+            raise ValidationError("That username is taken")
+
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField(
+        validators=[InputRequired(), Length(min=4, max=20)],
+        render_kw={"placeholder": "Password"},
+    )
+    submit = SubmitField("Login")
 
 
 @app.route("/")
@@ -25,14 +59,23 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+    return render_template("login.html", form=form)
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for("login"))
+    return render_template("register.html", form=form)
 
 
 if __name__ == "__main__":
